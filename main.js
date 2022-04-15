58e0dc4f1a077c9082f1abc6463df7252e06a5d8
Tyler Durden <tylerdurden1412@gmail.com>
2022-04-15 17:52:52 +0300

    add json

--- main.js
+++ main.js
@@ -24,3 +24,21 @@ addBookToLibrary(book4);
 
 console.log(myLibrary)
 
+const form = document.getElementById("book-form");
+
+form.addEventListener("submit",(e) => {
+    e.preventDefault()
+    var res = document.querySelector('input[name="readit"]:checked').value;
+    if (res === "yes") {
+        var book = new Book(author.value,title.value,pages.value,true)
+    }
+    else if (res === "no") {
+        var book = new Book(author.value,title.value,pages.value,false)
+    }
+    addBookToLibrary(book);
+    form.reset()
+    const panel = document.getElementById("panel")
+    const newDiv = document.createElement("div")
+    panel.appendChild(newDiv)
+    newDiv.innerHTML = JSON.stringify(myLibrary, null, 2);
+})
