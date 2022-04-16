c42346e708ec5f3f77682af8d4caaf7dd0361d62
Tyler Durden <tylerdurden1412@gmail.com>
2022-04-16 15:16:21 +0300

    css grid

--- main.js
+++ main.js
@@ -13,32 +13,35 @@ class Book {
     }
 }
 
-var book1 = new Book("whateaaver","whatevsserrr",123,true);
-addBookToLibrary(book1);
-var book2 = new Book("whatever","whatevferrr",123,true);
-addBookToLibrary(book2);
-var book3 = new Book("whatever","whatedsafverrr",123,true);
-addBookToLibrary(book3);
-var book4 = new Book("whataaever","whateverrr",123,true);
-addBookToLibrary(book4);
+// var book1 = new Book("whateaaver","whatevsserrr",123,true);
+// addBookToLibrary(book1);
+// var book2 = new Book("whatever","whatevferrr",123,true);
+// addBookToLibrary(book2);
+// var book3 = new Book("whatever","whatedsafverrr",123,true);
+// addBookToLibrary(book3);
+// var book4 = new Book("whataaever","whateverrr",123,true);
+// addBookToLibrary(book4);
 
-console.log(myLibrary)
+// console.log(myLibrary)
 
 const form = document.getElementById("book-form");
+const panel = document.getElementById("panel")
+// panel.innerHTML = JSON.stringify(myLibrary, null, 2);
 
 form.addEventListener("submit",(e) => {
     e.preventDefault()
     var res = document.querySelector('input[name="readit"]:checked').value;
     if (res === "yes") {
-        var book = new Book(author.value,title.value,pages.value,true)
+        var book = new Book(author.value,title.value,pages.value,"Yes")
     }
     else if (res === "no") {
-        var book = new Book(author.value,title.value,pages.value,false)
+        var book = new Book(author.value,title.value,pages.value,"No")
     }
     addBookToLibrary(book);
     form.reset()
-    const panel = document.getElementById("panel")
     const newDiv = document.createElement("div")
-    panel.appendChild(newDiv)
-    newDiv.innerHTML = JSON.stringify(myLibrary, null, 2);
+    newDiv.setAttribute('class','cards')
+    newDiv.innerHTML = `Author:${book.author}<br>Title:${book.title}<br>
+    Pages:${book.nPages}<br>Read It:${book.isRead}`
+    panel.appendChild(newDiv)   
 })
