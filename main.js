7327dc9a02858cbd82680b2e86385b6a44cbb759
Tyler Durden <tylerdurden1412@gmail.com>
2022-04-21 14:58:09 +0300

    toggle button

--- main.js
+++ main.js
@@ -1,10 +1,58 @@
-12var myLibrary = [];
+var myLibrary = [];
 
 function addBookToLibrary(obj) {
     myLibrary.push(obj);
 }
 function deleteObj(obj) {
     delete obj
+}
+function res() {
+    var res = document.querySelector('input[name="readit"]:checked').value;
+    if (res === "yes") {      
+        return new Book(author.value,title.value,pages.value,"Yes")
+    }
+    else if (res === "no") {
+        return new Book(author.value,title.value,pages.value,"No")
+    }
+}
+function createDiv(book) {
+    var newDiv = document.createElement("div")
+    newDiv.setAttribute('class','cards')
+    newDiv.setAttribute('id',`card${i}`)
+    newDiv.setAttribute('data-init',i);
+    newDiv.innerHTML = `Author:${book.author}<br>Title:${book.title}<br>
+    Pages:${book.nPages}<br>Read It:${book.isRead}<br>`
+    return newDiv
+}
+function createBtn() {
+    var newBtn = document.createElement("button")
+    newBtn.setAttribute('id',"delBtn")
+    newBtn.setAttribute('data-init',i);
+    return newBtn;
+}
+function createSvg() {
+    var newSvg = document.createElementNS("http://www.w3.org/2000/svg","svg")
+    newSvg.setAttribute("style","width:24px;height:24px");
+    newSvg.setAttribute("viewBox","0 0 24 24");
+    var newPath = document.createElementNS("http://www.w3.org/2000/svg","path")
+    newPath.setAttribute("fill","currentColor")
+    newPath.setAttribute("d","M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z")
+    newSvg.appendChild(newPath)
+    return newSvg
+}
+function createToggle(isRead) {
+    var newToggle = document.createElement("label");
+    newToggle.setAttribute("class","switch")
+    var input = document.createElement("input")
+    input.setAttribute("type","checkbox")
+    if (isRead === "Yes") {
+        input.setAttribute("checked","checked")
+    }
+    newToggle.appendChild(input)
+    var span = document.createElement("span")
+    span.setAttribute("class","slider round")
+    newToggle.appendChild(span);
+    return newToggle;
 }
 class Book {
     constructor(author,title,nPages,isRead) {
@@ -15,44 +63,22 @@ class Book {
     }
 }
 
-// var book1 = new Book("whateaaver","whatevsserrr",123,true);
-// addBookToLibrary(book1);
-// var book2 = new Book("whatever","whatevferrr",123,true);
-// addBookToLibrary(book2);
-// var book3 = new Book("whatever","whatedsafverrr",123,true);
-// addBookToLibrary(book3);
-// var book4 = new Book("whataaever","whateverrr",123,true);
-// addBookToLibrary(book4);
-
-// console.log(myLibrary)
-
 const form = document.getElementById("book-form");
 const panel = document.getElementById("panel")
 var i = 0;
-// panel.innerHTML = JSON.stringify(myLibrary, null, 2);
 
 form.addEventListener("submit",(e) => {
     e.preventDefault()
-    var res = document.querySelector('input[name="readit"]:checked').value;
-    if (res === "yes") {
-        var book = new Book(author.value,title.value,pages.value,"Yes")
-    }
-    else if (res === "no") {
-        var book = new Book(author.value,title.value,pages.value,"No")
-    }
+    var book = res()
     addBookToLibrary(book);
     form.reset()
-    const newDiv = document.createElement("div")
-    const newBtn = document.createElement("button")
-    newBtn.setAttribute('id',"delBtn")
-    newDiv.setAttribute('class','cards')
-    newDiv.setAttribute('id',`card${i}`)
-    newDiv.setAttribute('data-init',i);
-    newBtn.setAttribute('data-init',i);
-    newDiv.innerHTML = `Author:${book.author}<br>Title:${book.title}<br>
-    Pages:${book.nPages}<br>Read It:${book.isRead}`
-    newBtn.innerHTML = "Del"
+    var newDiv = createDiv(book);
+    var newToggle = createToggle(book.isRead);
+    var newBtn = createBtn();
+    var newSvg = createSvg();
+    newBtn.appendChild(newSvg);
     panel.appendChild(newDiv);
+    newDiv.appendChild(newToggle)
     newDiv.appendChild(newBtn);
     newBtn.addEventListener('click',(e) => {
         var child = document.getElementById(`card${newBtn.dataset.init}`)
